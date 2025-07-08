3b59b03e67bb66dfbb8ecda6bc3de87f19d523d5
knuckles240 <mateuspantera2409@gmail.com>
2025-07-07 22:35:54 -0300

    fix: estrutura

--- api/config/db.js
+++ api/config/db.js
@@ -1,19 +1,13 @@
-
-
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export async function connectDB() {
+export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://lucas:Batata007@comida.xhqw8jf.mongodb.net/?retryWrites=true&w=majority&appName=comida", {
-      dbName: 'appComida',
-      family: 4        // ← força IPv4
-    });
+    await mongoose.connect(process.env.MONGO_URI, { dbName: 'appComida' });
     console.log('MongoDB conectado ✔');
   } catch (err) {
-    console.error('Erro ao conectar MongoDB ❌', err);
+    console.error('Erro ao conectar com MongoDB ❌', err);
     process.exit(1);
   }
-}
-
+};
