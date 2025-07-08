d4732004e9053729d595a9ecb578847e7470c3b0
knuckles240 <mateuspantera2409@gmail.com>
2025-07-07 22:11:00 -0300

    fix: env corrigid

--- api/config/db.js
+++ api/config/db.js
@@ -1,13 +1,19 @@
+
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const connectDB = async () => {
+export async function connectDB() {
   try {
-    await mongoose.connect("mongodb+srv://lucas:Batata007@comida.xhqw8jf.mongodb.net/?retryWrites=true&w=majority&appName=comida", { dbName: 'appComida' });
+    await mongoose.connect("mongodb+srv://lucas:Batata007@comida.xhqw8jf.mongodb.net/?retryWrites=true&w=majority&appName=comida", {
+      dbName: 'appComida',
+      family: 4        // ← força IPv4
+    });
     console.log('MongoDB conectado ✔');
   } catch (err) {
-    console.error('Erro ao conectar com MongoDB ❌', err);
+    console.error('Erro ao conectar MongoDB ❌', err);
     process.exit(1);
   }
-};
+}
+
